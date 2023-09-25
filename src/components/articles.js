5752e4bba4d94ed7355f7d4820ff1470b2f82084
kad-er <tagkader@hotmail.fr>
2023-09-25 17:19:51 +0100

    corrected a duplicate class css

--- src/components/articles.js
+++ src/components/articles.js
@@ -22,7 +22,7 @@ const articles = ({articles}) => {
                     {article.content[0].substring(0,150)}...
                 </p>
                 <div>
-                    <Link className="text-indigo-500 inline-flex items-center mx-auto md:mb-2 lg:mb-0 mx-auto" to={`/articles/${article.name}`}>
+                    <Link className="text-indigo-500 inline-flex items-center  md:mb-2 lg:mb-0 mx-auto" to={`/articles/${article.name}`}>
                         Learn more
                     </Link>
                 </div>
